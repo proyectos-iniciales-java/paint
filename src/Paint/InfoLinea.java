70caf496f2d42ef4babb47ba25717b1ffe2308d5
LinaKrdozo <linakrdozo13@gmail.com>
2022-06-22 18:01:04 -0500

    Adding java exercise

---
+++ src/Paint/InfoLinea.java
@@ -0,0 +1,14 @@
+package Paint;
+
+import java.awt.Color;
+import java.awt.Shape;
+import java.io.Serializable;
+
+//clase que sera almacenada en mi arraylist
+public class InfoLinea implements Serializable {
+	
+	Shape linea;
+	
+	Color color;
+	
+}
